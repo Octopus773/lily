ec44835477ae702f727d055b6237ad8c79f8ace9
Octopus <clement.lebihan773@gmail.com>
2020-01-05 19:34:22 +0100

    less bugs with list_to_list but don't use a weird index

--- sources/lily_add_list_to_list.c
+++ sources/lily_add_list_to_list.c
@@ -10,11 +10,13 @@
 int lily_add_list_to_list(lily_list **first_dest, lily_list *first_src, int ind)
 {
     int size = lily_get_list_length(first_src);
+    int size_dest;
     lily_list *node;
 
     if (!first_dest || !*first_dest || !first_src)
         return (-1);
-    if (ind >= 0)
+    size_dest = lily_get_list_length(*first_dest);
+    if (ind >= 0 && ind <= size_dest)
         lily_inv_list(&first_src);
     for (int i = 0; i < size; i++) {
         node = first_src;
